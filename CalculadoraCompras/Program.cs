78a994c32edd287b611dadec3ad65983f83cb09b
brunofrancia310507-ux <brunofrancia310507@gmail.com>
2026-09-16 09:32:30 -0500

    Errores corregidos

--- CalculadoraCompras/Program.cs
+++ CalculadoraCompras/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿const double porcDescuento = 0.1;
+
+static void LeerDatos(double precio, int cantidad)
+{
+    Console.WriteLine("Ingrese precio:");
+    preciox = double.Parse(Console.ReadLine());
+    Console.WriteLine("Ingrese cantida:");
+    cantidadx = int.Parse(Console.ReadLine());
+}
+
+static double CalcularSubtotal(double preciox, int cantidadx)
+{
+    double sub_total = preciox * cantidadx;
+    return sub_total;
+}
+
+static double CalcularDescuento(double subtotalx)
+{
+    return subtotalx * porcDescuento;
+}
+
+static double CalcularTotal(double subtotalx, double descuentox)
+{
+    return subtotalx - descuentox;
+}
+
+Console.WriteLine("Bienvenido a la Calculadora de Compra");
+double precio =0;
+int cantidad =0;
+LeerDatos(ref precio,ref cantidad);
+
+//Comprobando
+double sub_total = CalcularSubtotal(precio, cantidad);
+System.Console.WriteLine($"Subtotal: {subtotal}");
+
+double descuento = CalcularDescuento(subtotal);
+System.Console.WriteLine($"Descuneto: {descuento}");
+
+double total = CalcularTotal(subtotal,descuento);
+System.Console.WriteLine($"Descuneto: {total}");
